932a4c0990e3a680fbd37838a72518dbd44c8f36
fahim ashhab <37286610+fahim-ash@users.noreply.github.com>
2021-05-29 09:47:27 +0600

    Create 1929.py

---
+++ 1929.py
@@ -0,0 +1,11 @@
+a,b,c,d=map(int,input().split())
+if a+b>c and a+c>b and b+c>a:
+    print("S")
+elif a+c>d and a+d>c and c+d>a:
+    print("S")
+elif b+c>d and b+d>c and d+c>b:
+    print("S")
+elif a+b>d and a+d>b and b+d>a:
+    print("S")
+else:
+    print("N")
